3e36dfacf79108dd6b17244289ebf20b2e23bd2e
Cristo González <alu0100694987@ull.edu.es>
2013-10-03 19:01:49 +0000

    Version inicial

---
+++ matriz.rb
@@ -0,0 +1,16 @@
+
+# Práctica 4 - LPP - L1 - G07
+# Autores:
+#      Cristo González Rodríguez
+#      Juvenal Santiso Hernández
+
+
+# Inicio del programa
+
+# Fase 1: Generación de matrices cuadradas
+
+
+# Fase 2: Suma de matrices
+
+
+# Fase 3: Multiplicación de matrices
